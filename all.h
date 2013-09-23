7f366756657b9d9bf6ec90869cfe9ae5ae5d3a20
Ben Usman <bigobangux@gmail.com>
2013-09-23 14:52:19 +0400

    all.h

---
+++ all.h
@@ -0,0 +1,15 @@
+#ifndef ALL_H_INCLUDED
+#define ALL_H_INCLUDED
+
+class Exp;
+class IdExp;
+class NumExp;
+class OpExp;
+class EseqExp;
+class Statement;
+class CompoundStatement;
+class AssignStm;
+class ExpList;
+
+
+#endif // ALL_H_INCLUDED
