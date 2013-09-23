d561598425c79ba3174922c87764616cb06a3d89
vima <vit312@gmail.com>
2013-09-23 14:52:28 +0400

    stm

--- Statement.cpp
+++ Statement.cpp
@@ -1,57 +1,24 @@
-#include <assert.h>
-#include <string>
+#include "stm.h"
 #include "ExpList.h"
 #include "Exp.h"
 
-using namespace std;
+CompoundStatement::CompoundStatement( const Statement* first, const Statement* second ) :
+firstStm( first ), secondStm( second )
+{
+    assert( firstStm != 0 );
+    assert( secondStm != 0 );
+}
 
-class Statement {
-public:
-	// Виртуальный деструктор т.к. предполагается наследование
-	virtual ~Statement() {}
-};
+AssignStm::AssignStm( const string id, const Exp* exp ) :
+id( id ), exp( exp )
+{
+    assert( id != "" );
+    assert( exp != 0 );
+}
 
-// Правило CompoundStm
-class CompoundStatement::CompoundStatement : public Statement {
-public:
-	CompoundStatement( const Statement* first, const Statement* second ) :
-	firstStm( first ), secondStm( second )
-	{
-		assert( firstStm != 0 );
-		assert( secondStm != 0 );
-	}
-
-private:
-	const Statement* const firstStm;
-	const Statement* const secondStm;
-};
-
-// AssignStm
-class AssignStm : public Statement {
-public:
-	AssignStm( const string id, const Exp* exp ) :
-	id( id ), exp( exp )
-	{
-		assert( id != "" );
-		assert( exp != 0 );
-	}
-
-private:
-	const string id;
-	const Exp* const exp;
-};
-
-// PrintStm
-class PrintStm : public Statement {
-public:
-	PrintStm( const ExpList* expList) :
-	expList( expList )
-	{
-		assert( expList != 0 );
-	}
-
-private:
-	const ExpList* expList;
-};
-
+PrintStm::PrintStm( const ExpList* expList) :
+expList( expList )
+{
+    assert( expList != 0 );
+}
 
