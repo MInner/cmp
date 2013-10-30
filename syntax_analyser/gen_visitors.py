7d3966f4242a66e1ca6d74c8f7ffe5e994b9e25a
Ben Usman <bigobangux@gmail.com>
2013-10-30 21:14:37 +0400

    generators for all

--- syntax_analyser/gen_visitors.py
+++ syntax_analyser/gen_visitors.py
@@ -1,11 +1,24 @@
 f = open('cl.txt')
 
 for line in f:
-	classname = line.strip()
+	classline = line.strip()
+	first = classline.split("|")[0]
+	classname = first.split(" ")[0]
+	tovisit = first.strip().split()[1:]
+	tocout = classline.split("|")[1].strip().split()
+
+	# (!!!) virtual functions 
 	# print "virtual int visit(const %s* n) = 0;" % classname
+	
+	# (!!!) functions implementations
+	coutexpr = " << ".join(tocout)
+	visitexpr = '\n'.join(['\t\tn->%s->Accept(this);' % visel for visel in tovisit])
 	print """
-int visit(const %s* n)
-{
-	std::cout << "%s: " << std::endl;
-	return 0;
-}""" % (classname, classname)
+	int visit(const %s* n)
+	{
+		std::cout << "%s: (" << %s << ")" << std::endl;
+
+%s
+
+		return 0;
+	}""" % (classname, classname, coutexpr, visitexpr)
