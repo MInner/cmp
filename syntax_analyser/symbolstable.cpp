98dfc25800bbf0cbddf24f92b4b2755dfc192e1c
vima <vit312@gmail.com>
2013-11-07 15:01:02 +0300

    *& -> *

--- syntax_analyser/symbolstable.cpp
+++ syntax_analyser/symbolstable.cpp
@@ -2,12 +2,22 @@
 #include <vector>
 #include "interfaces.h"
 
-const ClassInfo& ClassTable::addClass(const Symbol*& name){
+const ClassInfo* ClassTable::addClass(const Symbol* name, const Symbol* parentName){
 	for(int i = 0; i < classes.size(); i++){
-		if(classes.at(i).name == name){
+		if(classes.at(i)->name == name){
 			throw "class already exists";
 		}
 	}
-	ClassInfo a(name);
+	ClassInfo* a = new ClassInfo(name, parentName);
 	classes.push_back(a);
 }
+
+const ClassInfo* ClassTable::getClass(const Symbol* name){
+	for(int i = 0; i < classes.size(); i++){
+		if(classes.at(i)->name == name){
+			return classes.at(i);
+		}
+	}
+	throw "there is no such class";
+}
+
--- syntax_analyser/symbolstable.h
+++ syntax_analyser/symbolstable.h
@@ -4,11 +4,11 @@
 
 class ClassTable {
 	public:
-		std::vector<ClassInfo> classes;
+		std::vector<ClassInfo*> classes;
 
 		
-		const ClassInfo& addClass(const Symbol*& name);
-		const ClassInfo& getClass(const Symbol*& name);
+		const ClassInfo* addClass(const Symbol* name, const Symbol* parentName);
+		const ClassInfo* getClass(const Symbol* name);
 
 };
 
@@ -16,14 +16,15 @@ class ClassInfo {
 
 	public:
 		Symbol* name;
+		Symbol* parentName;
 		std::vector<VarInfo> fields;
 		std::vector<MethodInfo> methods;
 
-		ClassInfo(const Symbol* name);
-		const VarInfo& addField(const Symbol*& name);
-		const VarInfo& getField(const Symbol*& name);
-		const MethodInfo& addMethod(const Symbol*& name);
-		const MethodInfo& getMethod(const Symbol*& name);
+		ClassInfo(const Symbol* name, const Symbol* parentName);
+		const VarInfo* addField(const Symbol* name);
+		const VarInfo* getField(const Symbol* name);
+		const MethodInfo* addMethod(const Symbol* name);
+		const MethodInfo* getMethod(const Symbol* name);
 };
 
 class MethodInfo {
@@ -34,10 +35,10 @@ class MethodInfo {
 		IType* returnType;	
 
 	
-		const VarInfo& addParam(const Symbol*& name);
-		const VarInfo& getParam(const Symbol*& name);
-		const VarInfo& addLocalVar(const Symbol*& name);
-		const VarInfo& getLocalVar(const Symbol*& name);
+		const VarInfo* addParam(const Symbol* name);
+		const VarInfo* getParam(const Symbol* name);
+		const VarInfo* addLocalVar(const Symbol* name);
+		const VarInfo* getLocalVar(const Symbol* name);
 };
 
 
