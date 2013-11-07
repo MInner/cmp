07560f2a9389a3da0761d0ebc1b70af4de9525dc
vima <vit312@gmail.com>
2013-11-07 15:25:16 +0300

    ClassInfo

--- syntax_analyser/symbolstable.cpp
+++ syntax_analyser/symbolstable.cpp
@@ -21,3 +21,43 @@ const ClassInfo* ClassTable::getClass(const Symbol* name){
 	throw "there is no such class";
 }
 
+ClassInfo::ClassInfo(const Symbol* name_, const Symbol* parentName_):
+	name(name_), parentName(parentName_){}
+
+const VarInfo* ClassInfo::addField(const Symbol* name, const IType* type){
+	for(int i = 0; i < fields.size(); i++){
+		if(fields.at(i)->name == name){
+			throw "field already exists";
+		}
+	}
+	VarInfo* a = new VarInfo(name, type);
+	fields.push_back(a);
+}
+
+const VarInfo* ClassInfo::getField(const Symbol* name){
+	for(int i = 0; i < fields.size(); i++){
+		if(fields.at(i)->name == name){
+			return fields.at(i);
+		}
+	}
+	throw "there is no such field";
+}
+
+const MethodInfo* ClassInfo::addMethod(const Symbol* name,const IType* returnType){
+	for(int i = 0; i < methods.size(); i++){
+		if(methods.at(i)->name == name){
+			throw "method already exists";
+		}
+	}
+	MethodInfo* a = new MethodInfo(name, returnType);
+	methods.push_back(a);
+}
+
+const MethodInfo* ClassInfo::getMethod(const Symbol* name){
+	for(int i = 0; i < methods.size(); i++){
+		if(methods.at(i)->name == name){
+			return methods.at(i);
+		}
+	}
+	throw "there is no such method";
+}
--- syntax_analyser/symbolstable.h
+++ syntax_analyser/symbolstable.h
@@ -5,7 +5,6 @@
 class ClassTable {
 	public:
 		std::vector<ClassInfo*> classes;
-
 		
 		const ClassInfo* addClass(const Symbol* name, const Symbol* parentName);
 		const ClassInfo* getClass(const Symbol* name);
@@ -13,28 +12,28 @@ class ClassTable {
 };
 
 class ClassInfo {
-
 	public:
-		Symbol* name;
-		Symbol* parentName;
-		std::vector<VarInfo> fields;
-		std::vector<MethodInfo> methods;
+		const Symbol* name;
+		const Symbol* parentName;
+		std::vector<VarInfo*> fields;
+		std::vector<MethodInfo*> methods;
 
 		ClassInfo(const Symbol* name, const Symbol* parentName);
-		const VarInfo* addField(const Symbol* name);
+		const VarInfo* addField(const Symbol* name_, const IType* type_);
 		const VarInfo* getField(const Symbol* name);
-		const MethodInfo* addMethod(const Symbol* name);
+		const MethodInfo* addMethod(const Symbol* name,const IType* returnType);
 		const MethodInfo* getMethod(const Symbol* name);
 };
 
 class MethodInfo {
 	public:
-		Symbol* name;
-		std::vector<VarInfo> params;
-		std::vector<VarInfo> localVars;
-		IType* returnType;	
+		const Symbol* name;
+		std::vector<VarInfo*> params;
+		std::vector<VarInfo*> localVars;
+		const IType* returnType;	
 
-	
+		MethodInfo(const Symbol* name_, const IType* returnType_):
+			name(name_), returnType(returnType_){};
 		const VarInfo* addParam(const Symbol* name);
 		const VarInfo* getParam(const Symbol* name);
 		const VarInfo* addLocalVar(const Symbol* name);
@@ -44,7 +43,8 @@ class MethodInfo {
 
 class VarInfo {
 	public:
-		Symbol* name;
-		IType* type;	
-	
+		const Symbol* name;
+		const IType* type;		
+		VarInfo(const Symbol* name_, const IType* type_):
+			name(name_), type(type_){};
 };
