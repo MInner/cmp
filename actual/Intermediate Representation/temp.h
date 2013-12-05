af332ca22a5197d75a169182f41fb73562bee56e
vima <vit312@gmail.com>
2013-12-05 14:55:51 +0400

    ++Temp; ++translate

--- actual/Intermediate Representation/temp.h
+++ actual/Intermediate Representation/temp.h
@@ -1,16 +1,43 @@
-namespace Temp;
+#pragma once
 
-class Temp {
-    public Temp();
-};
+#include "symbol.h"
 
-class Label 
-{
-public:
-    Label();
-    Label(const Symbol* s);
+namespace Temp{
 
-};
-//public class TempList {...}
-//public class LabelList {...}
+    class Temp 
+    {
+    public: 
+        Temp(){
+            std::stringstream s;
+            s << "T" << currId;
+            name.str();
+            currId++;
+        };
+        Temp(const std::string s){
+            name = Symbol::getSymbol(s).getStr();
+        };
+        
+        std::string name;
+        static int currId;
+    }; // Temp::currId = 1;
+
+    class Label 
+    {
+    public:
+        Label(){
+            std::stringstream s;
+            s << "L" << currId;
+            name.str();
+            currId++;
+        };
+        Label(const std::string s){
+            name = Symbol::getSymbol(s).getStr();
+        };
+        
+        std::string name;
+        static int currId;
+    };
+    
+    //public class TempList {...}
+    //public class LabelList {...}
 }
--- actual/Intermediate Representation/translate.h
+++ actual/Intermediate Representation/translate.h
@@ -1,7 +1,66 @@
-namespace Translate;
-class ISuntreeWrapper {
+namespace Translate{
+
+class Exp 
+{
 public:
-	const Tree::Exp* ToExp() const = 0; // как Expr
-	const Tree::Stm* ToStm() const = 0; // как Stm
-	const Tree::Stm* ToConditional(const Temp::Label* t, const Temp::Label* f) const = 0; // как if/jump
+    const IRTree::Exp* ToExp() const = 0;
+    const IRTree::Stm* ToStm() const = 0;
+    const IRTree::Stm* ToConditional(const Temp::Label* t, const Temp::Label* f) const =0;     
+}
+
+class IExp : public Exp {
+public:
+    IExp(const Exp* e) {exp=e;}
+    
+    const IRTree.Exp* ToExp() {return exp;}
+    const IRTree.Stm* ToStm() {return new IRTree.EXP(exp);}
+    //const IRTree::Stm* ToConditional(const Temp::Label* t, const Temp::Label* f) { ... ?... }
+    
+    const Exp* exp;
+}
+class IStm :public Exp {
+public:
+    IStm(const Stm* s) {stm=s;}
+    
+    //IRTree.Exp ToExp() { ... ?... }
+    const Stm* ToStm() {return stm;}
+    // IRTree.Stm ToConditional(Label t, Label f) { ... ?... }
+    
+    const Stm* stm;
+}
+
+class ICond :public Exp {
+public:
+    const IRTree.Exp* ToExp() {
+    Temp::Temp r;
+    Temp::Label t;
+    Temp::Label f;
+    
+    return new IRTree.ESEQ(
+        new IRTree.SEQ(new IRTree.MOVE(new IRTree.TEMP(r),
+        new IRTree.CONST(1)),
+        new IRTree.SEQ(ToConditional(t,f),
+        new IRTree.SEQ(new IRTree.LABEL(f),
+        new IRTree.SEQ(new IRTree.MOVE(new IRTree.TEMP(r),
+        new IRTree.CONST(0)),
+        new IRTree.LABEL(t))))),
+        new IRTree.TEMP(r)
+    );
+    }
+    const Stm* ToStm() { ... }
+    abstract IRTree.Stm ToConditional(Label t, Label f);
+}
+
+
+
+
+
+
+class ISunIRTreeWrapper {
+public:
+	const IRTree::Exp* ToExp() const = 0; // как Expr
+	const IRTree::Stm* ToStm() const = 0; // как Stm
+	const IRTree::Stm* ToConditional(const Temp::Label* t, const Temp::Label* f) const = 0; // как if/jump
+}
+
 }
