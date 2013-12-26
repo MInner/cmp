a5e42298965b8b32d0f853036ffb83d95b12b420
vima <vit312@gmail.com>
2013-12-26 14:07:45 +0400

    viz refactor

--- actual/irtreeprintvisitor.h
+++ actual/irtreeprintvisitor.h
@@ -11,61 +11,63 @@ namespace IRTree
 class IRTreePrintVisitor : public ITreeVisitor 
 {
 public:
-	IRTreePrintVisitor( std::ostream& stream ): stream( stream ),
-		currentCluster( 0 ), currentNode( 0 ) {}
+	IRTreePrintVisitor(std::ostream& out_): out(out_),
+		clust(0), node(0) {}
 		
 	virtual ~IRTreePrintVisitor() {}
 
 	int visit(const CodeFragment* codeFragment)
 	{
-		currentCluster = 0;
-		currentNode = 0;
-		stream << "graph \"\" {" << std::endl;
-		for( auto currentCodeFragment = codeFragment; currentCodeFragment != nullptr; currentCodeFragment = currentCodeFragment->next ) {
-			stream << "subgraph cluster" << currentCluster++ << " {" << std::endl;
-			stream << "label=\"" << currentCodeFragment->frame->getName()->name << "\"" << std::endl;
+		clust = 0;
+		node = 0;
+		out << "graph \"\" {" << std::endl;
+		for(auto currentCodeFragment = codeFragment; 
+		        currentCodeFragment != nullptr; currentCodeFragment = currentCodeFragment->next ) 
+		{
+			out << "subgraph cluster" << clust++ << " {" << std::endl;
+			out << "label=\"" << currentCodeFragment->frame->getName()->name << "\"" << std::endl;
 			currentCodeFragment->retval->Accept( this );
-			stream << "}" << std::endl;
+			out << "}" << std::endl;
 		}
-		stream << "}" << std::endl;
+		out << "}" << std::endl;
 	}
 
 	int visit(const CONST* n)
 	{
-		stream << "n" << currentNode++ << " [shape=\"oval\",label=\"CONST(" << n->value << ")\"]" << std::endl;
+		out << "n" << node++ << " [shape=\"oval\",label=\"CONST(" << n->value << ")\"]" << std::endl;
 	}
 
 	int visit(const NAME* n)
 	{
-		stream << "n" << currentNode++ << " [shape=\"oval\",label=\"NAME(" << n->label->name << ")\"]" << std::endl;
+		out << "n" << node++ << " [shape=\"oval\",label=\"NAME(" << n->label->name << ")\"]" << std::endl;
 	}
 	
 	int visit(const TEMP* n)
 	{
-		stream << "n" << currentNode++ << " [shape=\"oval\",label=\"TEMP(" << n->temp->name << ")\"]" << std::endl;
+		out << "n" << node++ << " [shape=\"oval\",label=\"TEMP(" << n->temp->name << ")\"]" << std::endl;
 	}
 	
 	int visit(const BINOP* n)
 	{
-		static const char* names[] = { "+", "-", "*", "/", "&&", "||", "<<", ">>", "ARSHIFT", "^" };
-		const size_t myNodeNumber = currentNode++;
-		stream << "n" << myNodeNumber << " [shape=\"oval\",label=\"BINOP(" << names[n->binop] << ")\"]" << std::endl;
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
+		static const char* names[] = { "+", "-", "*", "/", "&&", "||", "<<", ">>", "ARSHIFT", "XOR" };
+		const long myNodeNumber = node++;
+		out << "n" << myNodeNumber << " [shape=\"oval\",label=\"BINOP(" << names[n->binop] << ")\"]" << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
 		n->left->Accept( this );
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
 		n->right->Accept( this );
 	}
 	
 	int visit(const MEM* n)
 	{
-		stream << "n" << currentNode++ << " [shape=\"oval\",label=\"MEM\"]" << std::endl;
-		stream << "n" << currentNode - 1 << " -- n" << currentNode << std::endl;
+		out << "n" << node++ << " [shape=\"oval\",label=\"MEM\"]" << std::endl;
+		out << "n" << node - 1 << " -- n" << node << std::endl;
 		n->exp->Accept( this );
 	}
 	
 	int visit(const CALL* n)
 	{
-		stream << "n" << currentNode++ << " [shape=\"component\",label=\"CALL(" << n->func->name << ")\"]" << std::endl;
+		out << "n" << node++ << " [shape=\"component\",label=\"CALL(" << n->func->name << ")\"]" << std::endl;
 		if( n->args != nullptr ) {
 			n->args->Accept( this );
 		}
@@ -73,86 +75,90 @@ public:
 	
 	int visit(const ESEQ* n)
 	{
-		size_t myNodeNumber = currentNode++;
-		stream << "n" << myNodeNumber << " [shape=\"oval\",label=\"ESEQ\"]" << std::endl;
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
+		long myNodeNumber = node++;
+		out << "n" << myNodeNumber << " [shape=\"oval\",label=\"ESEQ\"]" << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
 		n->stm->Accept( this );
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
 		n->exp->Accept( this );
 	}
 
 	int visit(const MOVE* n)
 	{
-		size_t myNodeNumber = currentNode++;
-		stream << "n" << myNodeNumber << " [shape=\"box\",label=\"MOVE\"]" << std::endl;
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
+		long myNodeNumber = node++;
+		out << "n" << myNodeNumber << " [shape=\"box\",label=\"MOVE\"]" << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
 		n->dst->Accept( this );
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
 		n->src->Accept( this );
 	}
 	
 	int visit(const EXP* n)
 	{
-		stream << "n" << currentNode++ << " [shape=\"box\",label=\"EXP\"]" << std::endl;
-		stream << "n" << currentNode - 1 << " -- n" << currentNode << std::endl;
+		out << "n" << node++ << " [shape=\"box\",label=\"EXP\"]" << std::endl;
+		out << "n" << node - 1 << " -- n" << node << std::endl;
 		n->exp->Accept( this );
 	}
 	
 	int visit(const JUMP* n)
 	{
-		size_t myNodeNumber = currentNode++;
-		stream << "n" << myNodeNumber << " [shape=\"box\",label=\"JUMP\"]" << std::endl;
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
+		long myNodeNumber = node++;
+		out << "n" << myNodeNumber << " [shape=\"box\",label=\"JUMP\"]" << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
 		n->exp->Accept( this );
 		for( auto labels = n->targets; labels != nullptr && labels->label != nullptr; labels = labels->rest ) {
-			stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
-			stream << "n" << currentNode++ << " [shape=\"cds\",label=\"" << labels->label->name << "\"]" << std::endl;
+			out << "n" << myNodeNumber << " -- n" << node << std::endl;
+			out << "n" << node++ << " [shape=\"cds\",label=\"" << labels->label->name << "\"]" << std::endl;
 		}
 	}
 	
 	int visit(const CJUMP* n)
 	{
 		static const char* names[] = { "==", "!=", "<", ">", "<=", ">=", "ULT", "ULE", "UGT", "UGE" };
-		size_t myNodeNumber = currentNode++;
-		stream << "n" << myNodeNumber << " [shape=\"box\",label=\"JUMP(" << names[n->relop] << ")\"]" << std::endl;
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
-		n->left->Accept( this );
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
-		n->right->Accept( this );
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
-		stream << "n" << currentNode++ << " [shape=\"cds\",label=\"" << n->iftrue->name << "\"]" << std::endl;
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
-		stream << "n" << currentNode++ << " [shape=\"cds\",label=\"" << n->iffalse->name << "\"]" << std::endl;
+		long myNodeNumber = node++;
+		out << "n" << myNodeNumber << " [shape=\"box\",label=\"JUMP(" << names[n->relop] << ")\"]" << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
+		n->left->Accept(this);
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
+		n->right->Accept(this);
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
+		out << "n" << node++ << " [shape=\"cds\",label=\"" << n->iftrue->name << "\"]" << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
+		out << "n" << node++ << " [shape=\"cds\",label=\"" << n->iffalse->name << "\"]" << std::endl;
 	}
 	
 	int visit(const SEQ* n)
 	{
-		size_t myNodeNumber = currentNode++;
-		stream << "n" << myNodeNumber << " [shape=\"box\",label=\"SEQ\"]" << std::endl;
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
-		n->left->Accept( this );
-		stream << "n" << myNodeNumber << " -- n" << currentNode << std::endl;
-		n->right->Accept( this );
+		long myNodeNumber = node++;
+		out << "n" << myNodeNumber << " [shape=\"box\",label=\"SEQ\"]" << std::endl;
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
+		n->left->Accept(this);
+		out << "n" << myNodeNumber << " -- n" << node << std::endl;
+		n->right->Accept(this);
 	}
 	
 	int visit(const LABEL* n)
 	{
-		stream << "n" << currentNode++ << " [shape=\"box\",label=\"LABEL(" << n->label->name << ")\"]" << std::endl;
+		out << "n" << node++ << " [shape=\"box\",label=\"LABEL(" << n->label->name << ")\"]" << std::endl;
 	}
 
 	int visit(const ExpList* ExpList)
 	{
-		size_t parentNode = currentNode - 1;
+		long parentNode = node - 1;
 		for( auto exp = ExpList; exp != nullptr && exp->head != nullptr; exp = exp->tail ) {
-			stream << "n" << parentNode << " -- n" << currentNode << std::endl;
-			exp->head->Accept( this );
+			out << "n" << parentNode << " -- n" << node << std::endl;
+			exp->head->Accept(this);
 		}
+	}
+	
+	int visit(const StmList* stmList) 
+	{
 	}
 
 private:
-	std::ostream& stream;
-	long currentCluster;
-	long currentNode;
+	std::ostream& out;
+	long clust;
+	long node;
 };
 
 }
